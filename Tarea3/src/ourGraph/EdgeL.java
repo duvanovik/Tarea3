358b81524245ea50c8d03f00545ea2e322fa0fa3
duvan <duvan@LAPTOP-46GOSO3A>
2020-11-19 21:06:02 -0500

    I add vertex and edge

---
+++ Tarea3/src/ourGraph/EdgeL.java
@@ -0,0 +1,29 @@
+package ourGraph;
+
+public class EdgeL<V, E extends Comparable<E>> {
+
+    private VertexL<V, E> v;
+    private E value;
+
+    public EdgeL(VertexL<V, E> v, E value){
+        this.v = v;
+        this.value = value;
+    }
+
+    public VertexL<V, E> getVertex() {
+        return v;
+    }
+
+    public void setVertex(VertexL<V, E> v) {
+        this.v = v;
+    }
+
+    public E getWeight() {
+        return value;
+    }
+
+    public void setWeight(E value) {
+        this.value = value;
+    }
+}
+
